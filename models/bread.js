c7465ccc3cdd43bf4ae0311926cb53cf28b2e27b
Anthony-Ghilardi <Ghilardi@unlv.nevada.edu>
2024-05-13 19:38:58 -0700

    Added mongoose helper method

--- models/bread.js
+++ models/bread.js
@@ -1,16 +1,15 @@
-// require mongoose
+// require mongoose 
 const mongoose = require('mongoose')
+// creating shorthand for the Schema constructor 
+const { Schema } = mongoose 
 
-// creating shorthand for the Schema constructor
-const { Schema } = mongoose
-
-// Schema
-const breadSchema = new Schema ({
+// schema
+const breadSchema = new Schema({
     name: { type: String, required: true },
     hasGluten: Boolean,
     image: { type: String, default: 'http://placehold.it/500x500.png' }
 })
 
-// Bread Model and export for the model
+// model and export 
 const Bread = mongoose.model('Bread', breadSchema)
 module.exports = Bread
